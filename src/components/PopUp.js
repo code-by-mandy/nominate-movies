70a8aa20741123bcdc1191325b80e89b362e0a0c
code-by-mandy <mandycodes2@gmail.com>
2021-05-23 13:07:20 -0400

    get checkbox state on modal close

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,16 +1,24 @@
 const PopUp = ( {close, noPopUp}) => {
+
+    const handleClose = () => {
+        close();
+
+        const checkBox = document.querySelector('.noPopUp');
+        if (checkBox.checked) {
+            noPopUp();
+        }
+    }
     return (
         <div className="modalBody">
             <h1>You're all set!</h1>
             <p>Happy watching!</p>
             <p>To nominate a different movie, remove a movie from your nominations list.</p>
-            <button className="close" onClick={close}>Close</button>
+            <button className="close" onClick={handleClose}>Close</button>
             <form name="popUpPreference" className="popUpPreference">
                 <label htmlFor="noPopUp" className="noPopUpLabel">Don't show this message again</label>
                 <input 
                     id="noPopUp" 
                     type="checkbox" 
-                    onClick={noPopUp}
                     className="noPopUp"
                 ></input>
             </form>
