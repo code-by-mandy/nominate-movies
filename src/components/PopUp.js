7a7abc076433da96341bd514cce24db238a000ad
code-by-mandy <mandycodes2@gmail.com>
2021-06-01 14:26:37 -0400

    simplify pop up checkbox

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,25 +1,18 @@
 const PopUp = ( {close, noPopUp}) => {
 
-    const handleClose = () => {
-        close();
-
-        const checkBox = document.querySelector('.noPopUp');
-        if (checkBox.checked) {
-            noPopUp();
-        }
-    }
     return (
         <div className="modalBody">
             <h1>You're all set!</h1>
             <p>Happy watching!</p>
             <p>To nominate a different movie, remove a movie from your nominations list.</p>
-            <button className="close" onClick={handleClose}>Close</button>
+            <button className="close" onClick={() => close()}>Close</button>
             <form name="popUpPreference" className="popUpPreference">
                 <label htmlFor="noPopUp" className="noPopUpLabel">Don't show this message again</label>
                 <input 
                     id="noPopUp" 
                     type="checkbox" 
                     className="noPopUp"
+                    onClick={() => noPopUp()}
                 ></input>
             </form>
         </div>       
